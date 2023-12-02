a4a0f024eba9c54c19f14edf0f2e96e1cb85ff36
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-02 14:25:37 +0300

    Day 2 P1 [Go]

---
+++ 2023/go/d02/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+const inputFile = "input.txt"
+
+func readLines() []string {
+	_, filename, _, _ := runtime.Caller(0)
+	file := filepath.Join(path.Dir(filename), inputFile)
+	f, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Trim(string(f), "\n")
+	return strings.Split(input, "\n")
+}
+
+type Cubes = map[string]int
+
+func parseLine(line string) (int, []Cubes) {
+	var rCube = regexp.MustCompile(`(\d+)\s*(red|green|blue)`)
+	var rNum = regexp.MustCompile(`\d+`)
+	split := strings.Split(line, ":")
+	gameNum, _ := strconv.Atoi(rNum.FindString(split[0]))
+	setstr := strings.Split(split[1], ";")
+
+	var results = []Cubes{}
+	for _, set := range setstr {
+		s := Cubes{}
+		for _, cube := range strings.Split(set, ",") {
+			match := rCube.FindStringSubmatch(cube)
+			n, _ := strconv.Atoi(match[1])
+			s[match[2]] = n
+		}
+		results = append(results, s)
+	}
+
+	return gameNum, results
+}
+
+func partOne(limits Cubes, lines []string) int {
+	result := 0
+
+	var checkIfPossible = func(game []Cubes) bool {
+		for _, set := range game {
+			for cube, used := range set {
+				if limits[cube] < used {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
+	for _, l := range lines {
+		gameNum, sets := parseLine(l)
+		isPossible := checkIfPossible(sets)
+		if isPossible {
+			result += gameNum
+			continue
+		}
+		fmt.Println(gameNum, "is not possible")
+	}
+	return result
+}
+
+func main() {
+	lines := readLines()
+	limits := Cubes{"red": 12, "green": 13, "blue": 14}
+	fmt.Println(partOne(limits, lines))
+}
