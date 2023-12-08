c693efe21e9bca89adcb94d1662f839a30ed36de
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-08 16:13:05 +0300

    Day 8 P2 [Go]

--- 2023/go/d08/main.go
+++ 2023/go/d08/main.go
@@ -36,7 +36,7 @@ func parseDirs(l string) (dir []int) {
 
 func parseSteps(lines []string) map[string][]string {
 	steps := make(map[string][]string)
-	re := regexp.MustCompile(`[A-Z]+`)
+	re := regexp.MustCompile(`[A-Z|0-9]+`)
 	for _, l := range lines {
 		r := re.FindAllString(l, -1)
 		steps[r[0]] = r[1:]
@@ -56,7 +56,73 @@ func PartOne(lines []string) int {
 	return count
 }
 
+func findLoop(start string, dirs []int, steps map[string][]string) (count int) {
+	zz := []string{}
+	var include = func(step string) bool {
+		for _, s := range zz {
+			if s == step {
+				return true
+			}
+		}
+		return false
+	}
+	step := start
+	for i := 0; ; i++ {
+		count++
+		d := dirs[i%len(dirs)]
+		step = steps[step][d]
+		if strings.HasSuffix(step, "Z") {
+			if len(zz) == 0 {
+				count = 0
+			}
+			if include(step) {
+				break
+			}
+			zz = append(zz, step)
+			continue
+		}
+	}
+	return count
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(nums ...int) int {
+	a, b, rest := nums[0], nums[1], nums[2:]
+	result := a * b / gcd(a, b)
+
+	for _, c := range rest {
+		result = lcm(result, c)
+	}
+
+	return result
+}
+
+func PartTwo(lines []string) int {
+	dirs := parseDirs(lines[0])
+	steps := parseSteps(lines[2:])
+
+	starts := []string{}
+	for key := range steps {
+		if strings.HasSuffix(key, "A") {
+			starts = append(starts, key)
+		}
+	}
+
+	loops := []int{}
+	for _, start := range starts {
+		loops = append(loops, findLoop(start, dirs, steps))
+	}
+	return lcm(loops...)
+}
+
 func main() {
 	lines := readLines()
 	fmt.Println(PartOne(lines))
+	fmt.Println(PartTwo(lines))
 }
