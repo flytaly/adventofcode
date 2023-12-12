5b5574b1f75ff5d54cb6a453d6db0cc4d88bf912
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-12 18:16:13 +0300

    Day 12 P1 [Go]

---
+++ 2023/go/d12/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+func readLines(inputFile string) []string {
+	_, filename, _, _ := runtime.Caller(0)
+	file := filepath.Join(path.Dir(filename), inputFile)
+	f, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Trim(string(f), "\n")
+	return strings.Split(input, "\n")
+}
+
+func parse(lines []string) ([]string, [][]int) {
+	rows := []string{}
+	numRows := [][]int{}
+	for _, line := range lines {
+		res := strings.Split(line, " ")
+		rows = append(rows, res[0])
+		nums := []int{}
+		for _, n := range strings.Split(res[1], ",") {
+			num, _ := strconv.Atoi(n)
+			nums = append(nums, num)
+		}
+		numRows = append(numRows, nums)
+	}
+	return rows, numRows
+}
+
+func BtoI(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func count(row string, nums []int) (res int) {
+	if len(row) == 0 {
+		return BtoI(len(nums) == 0)
+	}
+	if len(nums) == 0 {
+		return BtoI(!strings.Contains(row, "#"))
+	}
+
+	if row[0] == '.' || row[0] == '?' {
+		res += count(row[1:], nums)
+	}
+
+	if row[0] == '#' || row[0] == '?' {
+		n := nums[0]
+
+		if n > len(row) || strings.Contains(row[:n], ".") || (n < len(row) && row[n] == '#') {
+			return res
+		}
+
+		if n == len(row) && !strings.Contains(row, ".") {
+			return res + BtoI(len(nums) == 1)
+		}
+
+		res += count(row[n+1:], nums[1:])
+	}
+
+	return res
+}
+
+func PartOne(lines []string) {
+	rows, numRows := parse(lines)
+
+	total := 0
+	for i := 0; i < len(rows); i++ {
+		total += count(rows[i], numRows[i])
+	}
+
+	fmt.Println("Part 1:", total)
+}
+
+func main() {
+	var inputFile = "input.txt"
+	if len(os.Args) > 1 {
+		inputFile = os.Args[1]
+	}
+	lines := readLines(inputFile)
+	PartOne(lines)
+}
