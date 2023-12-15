bf86e961e2a83c7f464dada7c49feb69a7abb837
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-15 09:57:13 +0300

    Day 15 P2 [Go]

--- 2023/go/d15/main.go
+++ 2023/go/d15/main.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -20,19 +22,74 @@ func readLines(inputFile string) []string {
 	return strings.Split(input, "\n")
 }
 
+func hash(str string) (res int) {
+	for _, v := range str {
+		res += int(v)
+		res *= 17
+		res %= 256
+	}
+
+	return res
+}
+
 func PartOne(lines []string) {
 	total := 0
 	for _, step := range strings.Split(lines[0], ",") {
-		current := 0
-		for _, v := range step {
-			current += int(v)
-			current *= 17
-			current %= 256
+		total += hash(step)
+	}
+	fmt.Println("Part 1:", total)
+}
+
+type Lens struct {
+	label string
+	focal int
+}
+
+func remove(slice *[]Lens, lb string) {
+	for i, v := range *slice {
+		if v.label == lb {
+			*slice = append((*slice)[:i], (*slice)[i+1:]...)
 		}
-		total += current
+	}
+}
+
+func add(slice *[]Lens, ln Lens) {
+	for i, v := range *slice {
+		if v.label == ln.label {
+			(*slice)[i].focal = ln.focal
+			return
+		}
+	}
+	*slice = append(*slice, ln)
+}
+
+func PartTwo(lines []string) {
+	re := regexp.MustCompile(`(\w+)(-|=)(\d+)?`)
+
+	boxes := make([][]Lens, 256)
+	for _, step := range strings.Split(lines[0], ",") {
+		matches := re.FindStringSubmatch(step)
+		label, operation, focal := matches[1], matches[2], 0
+		if len(matches) == 4 {
+			focal, _ = strconv.Atoi(matches[3])
+		}
+		boxId := hash(label)
+
+		switch operation {
+		case "=":
+			add(&boxes[boxId], Lens{label, focal})
+		case "-":
+			remove(&boxes[boxId], label)
+		}
 	}
 
-	fmt.Println("Part 1:", total)
+	total := 0
+	for i, lens := range boxes {
+		for j, l := range lens {
+			total += (1 + i) * (j + 1) * (l.focal)
+		}
+	}
+	fmt.Println("Part 2:", total)
 }
 
 func main() {
@@ -42,4 +99,5 @@ func main() {
 	}
 	lines := readLines(inputFile)
 	PartOne(lines)
+	PartTwo(lines)
 }
