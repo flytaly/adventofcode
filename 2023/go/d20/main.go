25b5dbcb56720c397c1f3904ca2945a98d6eb8b1
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-20 10:26:29 +0300

    Day 20 P2 [Go]

--- 2023/go/d20/main.go
+++ 2023/go/d20/main.go
@@ -43,17 +43,25 @@ type Pulse struct {
 }
 
 func parse(input []string) map[string]Module {
-	m := make(map[string]Module)
+	modules := make(map[string]Module)
 	for _, v := range input {
 		s := strings.Split(v, " -> ")
 		id, dest := s[0], strings.Split(s[1], ", ")
 		if id == "broadcaster" {
-			m[id] = Module{tp: "broadcaster", dest: dest}
+			modules[id] = Module{tp: "broadcaster", dest: dest}
 			continue
 		}
-		m[id[1:]] = Module{tp: id[:1], dest: dest, on: false, memo: make(map[string]string)}
+		modules[id[1:]] = Module{tp: id[:1], dest: dest, on: false, memo: make(map[string]string)}
 	}
-	return m
+
+	for name, m := range modules {
+		for _, dest := range m.dest {
+			if modules[dest].tp == "&" {
+				modules[dest].memo[name] = "l"
+			}
+		}
+	}
+	return modules
 }
 
 func PartOne(lines []string) {
@@ -66,14 +74,6 @@ func PartOne(lines []string) {
 			return
 		}
 		sent[0]++
-	}
-
-	for name, m := range modules {
-		for _, dest := range m.dest {
-			if modules[dest].tp == "&" {
-				modules[dest].memo[name] = "l"
-			}
-		}
 	}
 
 	for i := 0; i < 1000; i++ {
@@ -127,6 +127,83 @@ func PartOne(lines []string) {
 	fmt.Println("Part 1:", sent, "=>", sent[0]*sent[1])
 }
 
+type LT map[string]int
+
+func (lt *LT) set(p Pulse, v int) {
+	for id, value := range *lt {
+		if p.to == id && p.tp == "l" && value == 0 {
+			(*lt)[id] = v
+			break
+		}
+	}
+}
+
+func (lt LT) allSet() bool {
+	for _, v := range lt {
+		if v == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PartTwo(lines []string) {
+	modules := parse(lines)
+
+	var loopTarget = LT{"tx": 0, "dd": 0, "nz": 0, "ph": 0} // input specific
+
+	for cycle := 1; !loopTarget.allSet(); cycle++ {
+		Q := []Pulse{{tp: "l", to: "broadcaster"}}
+		for len(Q) > 0 {
+			p := Q[0]
+			Q = Q[1:]
+			m := modules[p.to]
+			pulseType := p.tp
+			destType, dest := m.tp, m.dest
+
+			loopTarget.set(p, cycle)
+
+			if destType == "broadcaster" {
+				for _, v := range dest {
+					Q = append(Q, Pulse{tp: pulseType, to: v, from: "broadcaster"})
+				}
+			}
+
+			if destType == "%" { // flipflop
+				if p.tp == "h" {
+					continue
+				}
+				nextType := "h"
+				if m.on {
+					nextType = "l"
+				}
+				m.on = !m.on
+				modules[p.to] = m
+				for _, v := range dest {
+					Q = append(Q, Pulse{tp: nextType, to: v, from: p.to})
+				}
+			}
+
+			if destType == "&" { // conjunction
+				m.memo[p.from] = pulseType
+				nextType := "h"
+				if m.memoOnlyHigh() {
+					nextType = "l"
+				}
+				for _, v := range dest {
+					Q = append(Q, Pulse{tp: nextType, to: v, from: p.to})
+				}
+			}
+
+		}
+	}
+	res := 1
+	for _, v := range loopTarget {
+		res *= v
+	}
+	fmt.Println("Part 2:", res)
+}
+
 func main() {
 	var inputFile = "input.txt"
 	if len(os.Args) > 1 {
@@ -134,4 +211,5 @@ func main() {
 	}
 	lines := readLines(inputFile)
 	PartOne(lines)
+	PartTwo(lines)
 }
