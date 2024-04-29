fd1a353e38d915fd3e70527e49df64519d3cb3af
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-04-29 16:06:17 +0300

    2016 Day 09 [Go] Part 2

--- 2016/d09/main.go
+++ 2016/d09/main.go
@@ -25,6 +25,26 @@ func decompressV1(str string) (count int) {
 	return count
 }
 
+func decompressV2(str string) (count int) {
+	re := regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+
+	for i := 0; i < len(str); i++ {
+		if !re.MatchString(str[i:]) {
+			count++
+			continue
+		}
+		match := re.FindStringSubmatch(str[i:])
+		vals := utils.ToInts(match[1:])
+		size, repeat := vals[0], vals[1]
+		startIdx := i + len(match[0])
+		count += decompressV2(str[startIdx:startIdx+size]) * repeat
+
+		i = startIdx + size - 1
+	}
+
+	return count
+}
+
 func main() {
 	lines := []string{}
 	if len(os.Args) > 1 {
@@ -32,4 +52,5 @@ func main() {
 		lines = utils.ReadLines(inputFile)
 	}
 	fmt.Println("PartOne: ", decompressV1(lines[0]))
+	fmt.Println("PartOne: ", decompressV2(lines[0]))
 }
--- 2016/d09/main_test.go
+++ 2016/d09/main_test.go
@@ -16,4 +16,13 @@ func TestMain(t *testing.T) {
 			{"X(8x2)(3x3)ABCY", 18},
 		}.Run(t, decompressV1)
 	})
+
+	t.Run("p2", func(t *testing.T) {
+		utils.TestSuite[string, int]{
+			{"(3x3)XYZ", 9},
+			{"X(8x2)(3x3)ABCY", 20},
+			{"(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920},
+			{"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445},
+		}.Run(t, decompressV2)
+	})
 }
