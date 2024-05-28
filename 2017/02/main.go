a3fe4ea15f512650923dcc4e7d1449921696ee2a
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-05-28 12:02:32 +0300

    2017 Day 02 [Go] Part 2

--- 2017/02/main.go
+++ 2017/02/main.go
@@ -31,6 +31,28 @@ func P1(input []string) (sum int) {
 	return sum
 }
 
+func divisible(nums []int) (int, int) {
+	for i, n1 := range nums {
+		for _, n2 := range nums[i+1:] {
+			low, high := minmax([]int{n1, n2})
+			if high%low == 0 {
+				return low, high
+			}
+		}
+	}
+	return 2, 2
+}
+
+func P2(input []string) (sum int) {
+	re := regexp.MustCompile(`\s`)
+	for _, line := range input {
+		nums := utils.ToInts(re.Split(line, -1))
+		low, high := divisible(nums)
+		sum += high / low
+	}
+	return sum
+}
+
 func main() {
 	lines := []string{}
 	if len(os.Args) > 1 {
@@ -38,4 +60,5 @@ func main() {
 		lines = utils.ReadLines(inputFile)
 	}
 	fmt.Println("Part1: ", P1(lines))
+	fmt.Println("Part2: ", P2(lines))
 }
--- 2017/02/main_test.go
+++ 2017/02/main_test.go
@@ -16,4 +16,14 @@ func TestMain(t *testing.T) {
 		}
 		tests.Run(t, P1)
 	})
+	t.Run("p2", func(t *testing.T) {
+		tests := utils.TestSuite[[]string, int]{
+			{[]string{
+				"5 9 2 8",
+				"9 4 7 3",
+				"3 8 6 5",
+			}, 9},
+		}
+		tests.Run(t, P2)
+	})
 }
