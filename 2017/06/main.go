ea2e1e506a3d8ea47497764cef0b0fe1a8489e62
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-06-02 12:47:59 +0300

    2017 Day 06 [Go] Part 1

---
+++ 2017/06/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"aoc/utils"
+	"cmp"
+	"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func maxIndex[T cmp.Ordered](values []T) (index int) {
+	maxVal := values[0]
+	for i, v := range values {
+		if v > maxVal {
+			maxVal, index = v, i
+		}
+	}
+	return index
+}
+
+func reallocate(nums *[]int, index int) {
+	n := *nums
+	value := n[index]
+	n[index] = 0
+	for i := 1; i <= value; i++ {
+		n[(index+i)%len(n)] += 1
+	}
+}
+
+func P1(input []string) (count int) {
+	split := regexp.MustCompile(`\s`).Split(input[0], -1)
+	nums := utils.ToInts(split)
+
+	states := map[string]bool{}
+
+	for count = 1; ; count++ {
+		index := maxIndex(nums)
+		reallocate(&nums, index)
+		bytes, _ := json.Marshal(nums)
+		state := string(bytes)
+		if states[state] {
+			return count
+		}
+		states[state] = true
+	}
+}
+
+func main() {
+	lines := []string{"0 2 7 0"}
+	if len(os.Args) > 1 {
+		inputFile := os.Args[1]
+		lines = utils.ReadLines(inputFile)
+	}
+	fmt.Println("Part 1 =>", P1(lines))
+}
