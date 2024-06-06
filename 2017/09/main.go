3e9b6481c87b78ab0e4214c1b9b5a6f010937dfc
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-06-06 06:49:01 +0300

    2017 Day 09 [Go] Part 2

--- 2017/09/main.go
+++ 2017/09/main.go
@@ -36,6 +36,43 @@ func P1(input string) int {
 	return sum
 }
 
+func P2(input string) int {
+	isGarbage := false
+	nested, sum := 0, 0
+	garbageTotal, garbageCount := 0, 0
+	for i := 0; i < len(input); i++ {
+		if input[i] == '!' {
+			i++
+			continue
+		}
+
+		if isGarbage {
+			if input[i] == '>' {
+				isGarbage = false
+				garbageTotal += garbageCount
+				garbageCount = 0
+			} else {
+				garbageCount++
+			}
+			continue
+		}
+
+		switch input[i] {
+		case '<':
+			isGarbage = true
+		case '{':
+			nested++
+		case '}':
+			sum += nested
+			nested--
+
+		}
+
+	}
+
+	return garbageTotal
+}
+
 func main() {
 	lines := []string{
 		"{{<a!>},{<a!>},{<a!>},{<ab>}}",
@@ -45,4 +82,5 @@ func main() {
 		lines = utils.ReadLines(inputFile)
 	}
 	fmt.Println("Part 1 =>", P1(lines[0]))
+	fmt.Println("Part 2 =>", P2(lines[0]))
 }
