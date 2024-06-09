f4e2beab11d35402baafe315dbf991e00d472c26
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-06-09 20:04:47 +0300

    2017 Day 11 [Go] Part 2

--- 2017/11/main.go
+++ 2017/11/main.go
@@ -7,11 +7,34 @@ import (
 	"strings"
 )
 
-// about cube coordinate
+// about cube coordinates
 // https://www.redblobgames.com/grids/hexagons/#coordinates
 
 type Cube struct {
 	q, r, s int
+}
+
+func (c *Cube) moveTo(dir string) {
+	switch dir {
+	case "n":
+		c.r--
+		c.s++
+	case "s":
+		c.r++
+		c.s--
+	case "ne":
+		c.q++
+		c.r--
+	case "sw":
+		c.q--
+		c.r++
+	case "nw":
+		c.q--
+		c.s++
+	case "se":
+		c.q++
+		c.s--
+	}
 }
 
 func abs(a int) int {
@@ -26,30 +49,26 @@ func P1(input string) int {
 	moves := strings.Split(input, ",")
 
 	for _, move := range moves {
-		switch move {
-		case "n":
-			pos.r--
-			pos.s++
-		case "s":
-			pos.r++
-			pos.s--
-		case "ne":
-			pos.q++
-			pos.r--
-		case "sw":
-			pos.q--
-			pos.r++
-		case "nw":
-			pos.q--
-			pos.s++
-		case "se":
-			pos.q++
-			pos.s--
-		}
-
+		pos.moveTo(move)
 	}
 
 	return max(abs(pos.q), abs(pos.r), abs(pos.s))
+}
+
+func P2(input string) int {
+	pos := Cube{0, 0, 0}
+	moves := strings.Split(input, ",")
+	maxDist := 0
+
+	for _, move := range moves {
+		pos.moveTo(move)
+		dist := max(abs(pos.q), abs(pos.r), abs(pos.s))
+		if dist > maxDist {
+			maxDist = dist
+		}
+	}
+
+	return maxDist
 }
 
 func main() {
@@ -59,4 +78,5 @@ func main() {
 		lines = utils.ReadLines(inputFile)
 	}
 	fmt.Println("Part 1 =>", P1(lines[0]))
+	fmt.Println("Part 2 =>", P2(lines[0]))
 }
