2744c57ca36816bdd2cb3a0fc925729b8385ec2c
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-06-13 09:19:31 +0300

    2017 Day 15 [Go] Part 2

--- 2017/15/main.go
+++ 2017/15/main.go
@@ -20,6 +20,38 @@ func P1(a, b int) (count int) {
 	return count
 }
 
+func nextA(a int) int {
+	af := 16807
+	for {
+		a = (a * af) % 2147483647
+		if a%4 == 0 {
+			return a
+		}
+	}
+}
+
+func nextB(b int) int {
+	bf := 48271
+	for {
+		b = (b * bf) % 2147483647
+		if b%8 == 0 {
+			return b
+		}
+	}
+}
+
+func P2(a, b int) (count int) {
+	mask := (1 << 16) - 1
+	for i := 1; i < 5_000_000; i++ {
+		a, b = nextA(a), nextB(b)
+		if a&mask == b&mask {
+			count++
+		}
+	}
+
+	return count
+}
+
 func main() {
 	a := 65
 	b := 8921
@@ -30,4 +62,5 @@ func main() {
 	fmt.Println("a =", a)
 	fmt.Println("b =", b)
 	fmt.Println("Part 1 =>", P1(a, b))
+	fmt.Println("Part 2 =>", P2(a, b))
 }
