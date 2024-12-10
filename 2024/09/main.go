6f5ee5e305ac5aacaf49234ae98e21fdc24eeec4
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-12-10 14:21:06 +0300

    2024 Day 09 [Go] Part 2

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"aoc/utils"
 	"fmt"
+	"math"
 	"os"
+	"slices"
+	"time"
 )
 
 func PartOne(disk string) {
@@ -40,12 +43,92 @@ func PartOne(disk string) {
 		pos++
 	}
 
-	fmt.Println("Part  1:", sum)
+	fmt.Println("Part 1:", sum)
 }
 
+// returns the gap position and its size and removes the gap from the map
+func minValidGap(gaps map[int][]int, minSize int, maxPos int) (gapPos int, gapSize int, ok bool) {
+	gapPos = math.MaxInt
 
-func PartTwo(disk string) {
+	for size, positions := range gaps {
+		if size < minSize || len(positions) == 0 {
+			continue
+		}
+		if p := positions[len(positions)-1]; gapPos > p && p < maxPos {
+			gapPos = p
+			gapSize = size
+		}
+	}
 
+	if gapPos < math.MaxInt {
+		gg := gaps[gapSize]
+		gaps[gapSize] = gg[:len(gg)-1]
+		return gapPos, gapSize, true
+	}
+
+	return 0, 0, false
+}
+
+func PartTwo(diskMap string) {
+	disk := []int{}
+	gaps := map[int][]int{} // size to block's position map
+	//
+	id := 0
+
+	for i, value := range diskMap {
+		num := int(value - '0')
+		blockId := id
+		id += 1 - i%2
+		if i%2 != 0 {
+			blockId = 0
+			gaps[num] = append(gaps[num], len(disk))
+		}
+		disk = append(disk, slices.Repeat([]int{blockId}, num)...)
+	}
+	for i := range gaps {
+		slices.Reverse(gaps[i])
+	}
+
+	var startPos int
+	endPos := len(disk) - 1
+	for i := len(diskMap) - 1; i > 2; i-- {
+		size := int(diskMap[i] - '0')
+		if i%2 != 0 {
+			endPos = endPos - size
+			continue
+		}
+
+		startPos = endPos - size + 1
+
+		file := disk[startPos : endPos+1]
+		gapPos, gapSize, ok := minValidGap(gaps, size, startPos)
+		if !ok {
+			endPos = startPos - 1
+			continue
+		}
+		if gapLeft := gapSize - size; gapLeft > 0 {
+			gaps[gapLeft] = append(gaps[gapLeft], gapPos+size)
+			slices.SortFunc(gaps[gapLeft], func(a, b int) int {
+				return b - a
+			})
+		}
+
+		// insert the file into the gap.
+		disk = slices.Replace(disk, gapPos, gapPos+size, file...)
+
+		// clear the file
+		for j := startPos; j <= endPos; j++ {
+			disk[j] = 0
+		}
+
+		endPos = startPos - 1
+	}
+
+	sum := 0
+	for i, value := range disk {
+		sum += i * value
+	}
+	fmt.Println("Part 2:", sum)
 }
 
 func main() {
@@ -57,4 +140,7 @@ func main() {
 	}
 
 	PartOne(lines[0])
+	ts := time.Now()
+	PartTwo(lines[0])
+	fmt.Println(time.Since(ts))
 }
