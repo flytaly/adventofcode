2aa0066a7b209da69cb1776f7348f0c388ff62bf
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-12-13 12:22:28 +0300

    2024 Day 12 [Go] Part 2

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -10,7 +10,7 @@ import (
 
 var dirs = []image.Point{ToTop, ToLeft, ToRight, ToBottom}
 
-func regionBFS(grid Grid[string], visited map[image.Point]bool, start image.Point) (perimeter int, area int) {
+func regionDFS(grid Grid[string], visited map[image.Point]bool, start image.Point) (perimeter int, area int) {
 	visited[start] = true
 	area = 1
 	for _, d := range dirs {
@@ -22,7 +22,7 @@ func regionBFS(grid Grid[string], visited map[image.Point]bool, start image.Poin
 		if visited[next] {
 			continue
 		}
-		per, ar := regionBFS(grid, visited, next)
+		per, ar := regionDFS(grid, visited, next)
 		perimeter += per
 		area += ar
 	}
@@ -34,27 +34,87 @@ type Region struct {
 	Area int
 }
 
-func PartOne(lines []string) int {
+func PartOne(lines []string) (price int) {
 	grid := NewStringGrid(lines)
 	visited := map[image.Point]bool{}
 	regions := map[string][]Region{}
-
-	// perimeters := map[string][]int{}
-	// areas := map[string][]int{}
 
 	for p, label := range grid.PointsIter() {
 		if visited[p] {
 			continue
 		}
-		per, ar := regionBFS(grid, visited, p)
+		per, ar := regionDFS(grid, visited, p)
 		regions[label] = append(regions[label], Region{Per: per, Area: ar})
 	}
 
-	price := 0
 	for _, islands := range regions {
 		for _, reg := range islands {
 			price += reg.Per * reg.Area
 		}
+	}
+
+	return price
+}
+
+// count the number of vertices around a point, which can be a corner of a polygon
+func countVertices[T comparable](grid Grid[T], p image.Point) (vertices int) {
+	dd := []image.Point{ToLeft, ToTop, ToRight, ToBottom}
+	for i := range dd {
+		d1, d2 := dd[i], dd[(i+1)%4]
+		id := grid.At(p)
+		adj1, adj2, diag := p.Add(d1), p.Add(d2), p.Add(d1).Add(d2)
+		// outer corner
+		if grid.At(adj1) != id && grid.At(adj2) != id {
+			vertices++
+			continue
+		}
+		// inner corner
+		if grid.At(adj1) == id && grid.At(adj2) == id && grid.At(diag) != id {
+			vertices++
+		}
+	}
+
+	return vertices
+}
+
+// In a polygon, the number of vertices equals the number of edges.
+func regionBFS[T comparable](grid Grid[T], visited map[image.Point]bool, start image.Point) (edges int, area int) {
+	queue := []image.Point{start}
+	visited[start] = true
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+		area++
+		for _, d := range dirs {
+			next := p.Add(d)
+			if grid.At(next) == grid.At(p) && !visited[next] {
+				visited[next] = true
+				queue = append(queue, next)
+			}
+		}
+		edges += countVertices(grid, p)
+	}
+	return edges, area
+}
+
+func PartTwo(lines []string) (price int) {
+	type Polygon struct {
+		Edges int
+		Area  int
+	}
+	grid := NewStringGrid(lines)
+	visited := map[image.Point]bool{}
+	regions := []Polygon{}
+	for p, _ := range grid.PointsIter() {
+		if visited[p] {
+			continue
+		}
+		edges, ar := regionBFS(grid, visited, p)
+		regions = append(regions, Polygon{Edges: edges, Area: ar})
+	}
+
+	for _, reg := range regions {
+		price += reg.Edges * reg.Area
 	}
 
 	return price
@@ -80,4 +140,5 @@ func main() {
 	}
 
 	fmt.Println("Part 1: ", PartOne(lines))
+	fmt.Println("Part 2: ", PartTwo(lines))
 }
