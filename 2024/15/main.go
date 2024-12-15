fc0ab277342028a8e26453fd9fc88f5af493cc1b
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-12-15 14:48:43 +0300

    2024 Day 15 [Go] Part 2

--- 2024/15/main.go
+++ 2024/15/main.go
@@ -34,7 +34,7 @@ func parse(lines []string) (grid Grid[string], start image.Point, moves []image.
 	return grid, start, moves
 }
 
-func PartOne(lines []string, time, cols, rows int) (result int) {
+func PartOne(lines []string) (result int) {
 	grid, pos, moves := parse(lines)
 
 outer:
@@ -56,6 +56,75 @@ outer:
 
 	for p, v := range grid.PointsIter() {
 		if v == "O" {
+			result += p.Y*100 + p.X
+		}
+	}
+
+	return result
+}
+
+func expandGrid(g Grid[string]) Grid[string] {
+	grid := NewGrid[string]((g.Right+1)*2, g.Bottom+1)
+	for p, v := range g.PointsIter() {
+		v1, v2 := v, v
+		if v == "@" {
+			v2 = "."
+		} else if v == "O" {
+			v1, v2 = "[", "]"
+		}
+		grid.Set(image.Pt(p.X*2, p.Y), v1)
+		grid.Set(image.Pt(p.X*2+1, p.Y), v2)
+	}
+	return grid
+}
+
+func PartTwo(lines []string) (result int) {
+	grid1, pos, moves := parse(lines)
+	grid := expandGrid(grid1)
+	pos = image.Pt(pos.X*2, pos.Y)
+
+	pair := map[string]image.Point{
+		"[": image.Pt(1, 0),
+		"]": image.Pt(-1, 0),
+	}
+
+	grid.Set(pos, ".")
+outer:
+	for _, move := range moves {
+		transfer := map[image.Point]string{pos: "."}
+		checkQ := []image.Point{pos.Add(move)}
+		for len(checkQ) > 0 {
+			check := checkQ[0]
+			checkQ = checkQ[1:]
+			if _, ok := transfer[check]; ok {
+				continue
+			}
+			switch cell := grid.At(check); cell {
+			case "#":
+				continue outer
+			case "[", "]":
+				transfer[check] = cell
+				matching := check.Add(pair[cell])
+				transfer[matching] = grid.At(matching)
+				checkQ = append(checkQ, check.Add(move), matching.Add(move))
+			}
+		}
+
+		for p, v := range transfer {
+			if _, ok := transfer[p.Sub(move)]; !ok {
+				grid.Set(p, ".") // clear behind the first boxes
+			}
+			grid.Set(p.Add(move), v)
+		}
+
+		pos = pos.Add(move)
+	}
+
+	grid.Set(pos, "@")
+	fmt.Println(grid)
+
+	for p, v := range grid.PointsIter() {
+		if v == "[" {
 			result += p.Y*100 + p.X
 		}
 	}
@@ -93,5 +162,6 @@ func main() {
 		lines = utils.ReadLines(inputFile)
 	}
 
-	fmt.Println("Part 1: ", PartOne(lines, 100, 101, 103))
+	fmt.Println("Part 1: ", PartOne(lines))
+	fmt.Println("Part 2: ", PartTwo(lines))
 }
