6c0e5a93bcea766b2e7c57799a176f9711f7dc76
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-12-25 11:39:36 +0300

    2024 Day 23 [Go] Part 1

---
+++ 2024/23/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"aoc/utils"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func findSets(links map[string][]string, node string) [][]string {
+	rest := [][]string{}
+
+	for i, id1 := range links[node] {
+		for _, id2 := range links[node][i+1:] {
+			rest = append(rest, []string{id1, id2})
+		}
+	}
+
+	sets := [][]string{}
+	for _, names := range rest {
+		if slices.Contains(links[names[0]], names[1]) {
+			sets = append(sets, []string{node, names[0], names[1]})
+		}
+	}
+
+	return sets
+}
+
+func PartOne(lines []string) (result int) {
+	net := map[string][]string{}
+	for _, l := range lines {
+		ids := strings.Split(l, "-")
+		net[ids[0]] = append(net[ids[0]], ids[1])
+		net[ids[1]] = append(net[ids[1]], ids[0])
+	}
+
+	sets := map[[3]string]struct{}{}
+	for node := range net {
+		for _, set := range findSets(net, node) {
+			slices.Sort(set)
+			sets[[3]string(set)] = struct{}{}
+		}
+	}
+
+	var startWithT = func(s string) bool {
+		return strings.HasPrefix(s, "t")
+	}
+
+	for set, _ := range sets {
+		if slices.ContainsFunc(set[:], startWithT) {
+			result++
+		}
+	}
+
+	return result
+}
+
+func main() {
+	inputFile := "example.txt"
+	if len(os.Args) > 1 {
+		inputFile = os.Args[1]
+	}
+	lines := utils.ReadLines(inputFile)
+
+	fmt.Println("Part 1:", PartOne(lines))
+}
